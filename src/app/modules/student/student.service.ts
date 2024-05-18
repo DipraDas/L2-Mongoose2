34f9c91d3bc3308336166b003a98e1f9a5f801f3
Dipra <dipradas5940@gmail.com>
2024-05-18 18:59:31 +0600

    [8.9] -:> Insert A Student Data Into MongoDB

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -3,6 +3,7 @@ import { StudentModel } from "./student.model";
 
 const createStudentIntoDB = async (student: Student) => {
     const result = await StudentModel.create(student);
+    console.log('result--', result);
     return result;
 }
 
