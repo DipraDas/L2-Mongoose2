1327b413e57f7ac03ce73b74239fdc0bc24f715f
Dipra <dipradas5940@gmail.com>
2024-05-25 23:39:24 +0600

    [11.13] -:> Create Index Route And Module Summary

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,15 @@
-import express, { Application, NextFunction, Request, Response } from "express";
+import express, { Application, Request, Response } from "express";
 import cors from "cors";
-import { StudentRoutes } from "./app/modules/student/student.route";
-import { UserRoutes } from "./app/modules/user/user.route";
 import globalErrorHandler from "./app/middleware/globalErrorHandler";
 import notFound from "./app/middleware/notFound";
+import router from "./app/routes";
 const app: Application = express();
 
 // parser
 app.use(express.json());
 app.use(cors());
 
-// application route 
-app.use("/api/v1/student", StudentRoutes);
-app.use("/api/v1/users", UserRoutes);
+app.use('/api/v1', router);
 
 app.use(globalErrorHandler);
 app.use(notFound);
