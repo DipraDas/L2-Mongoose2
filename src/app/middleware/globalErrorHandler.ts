f33caf5f9c35ee33b11f784920cfe69994c859b8
Dipra <dipradas5940@gmail.com>
2024-06-01 00:22:48 +0600

    [14.3] -:> How To Convert Zod Error

--- src/app/middleware/globalErrorHandler.ts
+++ src/app/middleware/globalErrorHandler.ts
@@ -1,35 +1,47 @@
 import { ErrorRequestHandler } from "express";
-import { ZodError } from "zod";
+import { ZodError, ZodIssue } from "zod";
+import config from "../config";
+import { TErrorSource } from "../interface/error";
 
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
 
     // Setting default values
     let statusCode = err.statusCode || 500;
     let message = err.message || 'Something went wrong!'
-    console.log('++++++', err);
 
-    type TErrorSource = {
-        path: string | number;
-        message: string;
-    }[];
-
-    let errorSource: TErrorSource = [
+    let errorSources: TErrorSource = [
         {
             path: '',
             message: 'Something went wrong'
         }
     ]
 
+    const handleZodError = (err: ZodError) => {
+        const errorSources: TErrorSource = err.issues.map((issue: ZodIssue) => {
+            return {
+                path: issue.path[issue.path.length - 1],
+                message: issue.message
+            }
+        })
+        return {
+            statusCode,
+            message: 'Validation Error',
+            errorSources
+        }
+    }
+
     if (err instanceof ZodError) {
-        statusCode = 400,
-            message = 'Ami zod error'
+        const simplifiedError = handleZodError(err);
+        statusCode = simplifiedError?.statusCode;
+        message = simplifiedError?.message;
+        errorSources = simplifiedError?.errorSources
     }
 
     return res.status(statusCode).json({
         success: false,
         message,
-        errorSource,
-        error: err
+        errorSources,
+        stack: config.NODE_ENV === 'development' ? err?.stack : 'null'
     })
 }
 
