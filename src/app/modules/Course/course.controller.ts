e4ff97f986c4d5172554aabc33947976f41c9e50
Dipra <dipradas5940@gmail.com>
2024-06-03 23:40:45 +0600

    [15.3] -:> Create Course Service , Controller & Validation

---
+++ src/app/modules/Course/course.controller.ts
@@ -0,0 +1,78 @@
+import catchAsync from "../../utils/catchAsync";
+import SendResponse from "../../utils/sendResponse";
+import httpStatus from "http-status";
+import { CourseServices } from "./course.service";
+
+const createCourse = catchAsync(async (req, res) => {
+    const result = await CourseServices.createCourseIntoDB(
+        req.body
+    );
+
+    SendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: "Course created successfully",
+        data: result,
+    });
+});
+
+// const updateAcademicFaculty = catchAsync(async (req, res) => {
+//     const { facultyId } = req.params;
+//     const result = await CourseServices.(
+//         facultyId,
+//         req.body
+//     );
+
+//     SendResponse(res, {
+//         statusCode: httpStatus.OK,
+//         success: true,
+//         message: "Academic faculty updated successfully",
+//         data: result,
+//     });
+// });
+
+const getAllCourses = catchAsync(async (req, res) => {
+    const result = await CourseServices.getAllCoursesFromDB();
+
+    SendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: "All Courses retrived successfully",
+        data: result,
+    });
+});
+
+const getSingleCourse = catchAsync(async (req, res) => {
+    const { id } = req.params;
+
+    const result =
+        await CourseServices.getSingleCourseFromDB(id);
+
+    SendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: "Courses retrived successfully",
+        data: result,
+    });
+});
+
+const deleteCourse = catchAsync(async (req, res) => {
+    const { id } = req.params;
+
+    const result =
+        await CourseServices.deleteCourseFromDB(id);
+
+    SendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: "Courses retrived successfully",
+        data: result,
+    });
+});
+
+export const AcademicFacultyController = {
+    createCourse,
+    getAllCourses,
+    getSingleCourse,
+    deleteCourse
+};
