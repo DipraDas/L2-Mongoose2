635d3430a06e8dbc8616d67526e52c6a49acc0e9
Dipra <dipradas5940@gmail.com>
2024-06-11 21:57:45 +0600

    [17.3] -:> Implement Statics For Validation

--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -2,34 +2,36 @@ import httpStatus from "http-status";
 import AppError from "../../errors/appError";
 import { User } from "../user/user.model";
 import { TLoginUser } from "./auth.interface";
-import bcrypt from 'bcrypt';
 
 const loginUser = async (payload: TLoginUser) => {
 
-    const isUserExists = await User.findOne({ id: payload?.id });
-    if (!isUserExists) {
-        throw new AppError(httpStatus.NOT_FOUND, 'THis user is not found.')
+    console.log('ppp', payload);
+
+    const user = await User.isUserExistsByCustomId(payload.id);
+    console.log('>>>>>.', user);
+
+    if (!user) {
+        throw new AppError(httpStatus.NOT_FOUND, 'This user is not found.')
     }
 
     // checking if the user is already deleted? 
-    const isDeleted = isUserExists?.isDeleted;
-    if (isDeleted) {
-        throw new AppError(httpStatus.FORBIDDEN, 'This user is deleted.')
-    }
+    // const isDeleted = isUserExists?.isDeleted;
+    // if (isDeleted) {
+    //     throw new AppError(httpStatus.FORBIDDEN, 'This user is deleted.')
+    // }
 
-    // Checking if the user is blocked
+    // // Checking if the user is blocked
 
-    const userStatus = isUserExists?.status;
+    const userStatus = user?.status;
     if (userStatus === 'blocked') {
         throw new AppError(httpStatus.FORBIDDEN, 'This user is blocked.')
     }
 
-    const isPasswordMatched = await bcrypt.compare(
-        payload?.password,
-        isUserExists?.password
-    )
+    if (!await User.isPasswordMatched(payload?.password, user?.password)) {
+        throw new AppError(httpStatus.FORBIDDEN, 'Password not matched')
+    }
 
-    console.log(isPasswordMatched);
+    // console.log(isPasswordMatched);
 
     return {}
 }
--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -3,6 +3,7 @@ import { UserControllers } from './user.controller';
 import validateRequest from '../../utils/validateRequest';
 import { studentValidations } from '../student/student.zodValidator';
 import { facultyValidations } from '../Faculty/faculty.validation';
+import { AdminValidations } from '../Admin/admin.validation';
 
 const router = express.Router();
 
@@ -14,6 +15,11 @@ router.post('/create-student',
 router.post('/create-faculty',
     validateRequest(facultyValidations.createFacultyValidationSchema),
     UserControllers.createFaculty
+);
+
+router.post('/create-admin',
+    validateRequest(AdminValidations.createAdminValidationSchema),
+    UserControllers.createAdmin
 );
 
 export const UserRoutes = router;
